cebe772adc094ffbdaeda620a22d4e9316829859
Lifaet Hossain Zim <lifaethossain@gmail.com>
2023-07-06 21:41:03 +0600

    footer button fix

--- index.js
+++ index.js
@@ -24,3 +24,21 @@
         },
             'fast');
     });
+    $("#home1").click(function () {
+        $('html,body').animate({
+            scrollTop: $(".body-container").offset().top - 80
+        },
+            'fast');
+    });
+    $("#about1").click(function () {
+        $('html,body').animate({
+            scrollTop: $(".about-container").offset().top - 80
+        },
+            'fast');
+    });
+    $("#achivement1").click(function () {
+        $('html,body').animate({
+            scrollTop: $(".ach-container").offset().top - 80
+        },
+            'fast');
+    });
